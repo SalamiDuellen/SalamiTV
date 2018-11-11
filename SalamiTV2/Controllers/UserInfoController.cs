c864558ec47ad33d789b66145c765ad08965abff
Annica Alienus <anbj1001@student.miun.se>
2018-11-11 15:40:04 +0100

    Uppdaterat bootstraps och nugets. Lagt till Nugets för Identity-grejen. Lagt till controller och vy för UserInfo

---
+++ SalamiTV2/Controllers/UserInfoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SalamiTV2.Models;
+
+namespace SalamiTV2.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private SalamiDB db = new SalamiDB();
+
+        // GET: UserInfo
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.UserInfoes.ToListAsync());
+        }
+
+        // GET: UserInfo/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = await db.UserInfoes.FindAsync(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserInfo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,UserName,Password")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserInfoes.Add(userInfo);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = await db.UserInfoes.FindAsync(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,UserName,Password")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = await db.UserInfoes.FindAsync(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            UserInfo userInfo = await db.UserInfoes.FindAsync(id);
+            db.UserInfoes.Remove(userInfo);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
