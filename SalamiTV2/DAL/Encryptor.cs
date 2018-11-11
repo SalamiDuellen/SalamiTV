6b799d2d49dd53ab8d4e2d0c7e7513c42f3bd276
Annica Alienus <anbj1001@student.miun.se>
2018-11-11 14:56:38 +0100

    Flyttat databaskopplingen till DAL-mappen och lagt till encryptorklass för att hasha password

---
+++ SalamiTV2/DAL/Encryptor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SalamiTV2.DAL
+{
+    public class Encryptor
+    {
+        /// <summary>
+        /// Använder MD5 för kryptering av lösenord. Hashar texten och ändrar till 2 hexadecimal för varje byte i texten
+        /// https://sv.wikipedia.org/wiki/Hexadecimala_talsystemet
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+            return strBuilder.ToString();
+        }
+    }
+}
